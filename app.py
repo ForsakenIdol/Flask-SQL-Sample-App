113df7af3629d3c00c40499bcaf788c875edf29c
Lachlan <lachlandwhang@gmail.com>
2023-10-19 15:52:07 +0800

    Added retry logic to app.py.

--- app.py
+++ app.py
@@ -2,21 +2,32 @@ from flask import Flask, render_template
 from mysql import connector
 import os
 from prometheus_flask_exporter import PrometheusMetrics
+from time import sleep
 
 app = Flask(__name__)
 
-try:
-    cnx = connector.connect(user=os.environ["DB_USER"],
-                            password=os.environ["DB_PASSWORD"],
-                            host=os.environ["DB_HOST"],
-                            database=os.environ["DB_DATABASE"],
-                            port=3306)
-    cursor = cnx.cursor()
-    print("MySQL connection established.")
-except Exception as e:
-    print("MySQL database connection failed - proceeding without database connection.")
-    print (e)
-    cursor = None
+retry = 1
+connected = False
+max_retries = 10
+while (retry <= max_retries and not connected):
+    try:
+        cnx = connector.connect(user=os.environ["DB_USER"],
+                                password=os.environ["DB_PASSWORD"],
+                                host=os.environ["DB_HOST"],
+                                database=os.environ["DB_DATABASE"],
+                                port=3306)
+        cursor = cnx.cursor()
+        connected = True
+    except Exception as e:
+        print("MySQL connection failed: retry {} of {}. Retrying...".format(retry, max_retries))
+        print (e)
+        cursor = None
+        retry += 1
+        sleep(3)
+        
+
+if not connected:print("MySQL database connection failed - proceeding without database connection.")
+else: print("MySQL connection established.")
 
 flask_metrics = PrometheusMetrics(app, path="/mymetrics")
 
